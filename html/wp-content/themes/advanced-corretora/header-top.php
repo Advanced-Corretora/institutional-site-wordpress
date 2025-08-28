4952882d689473313128561c0fdd89231ebef63b
Joel Freitas <joel.santana@opah.com.br>
2025-08-28 17:29:05 -0300

    feat: Changed menu styles

--- html/wp-content/themes/advanced-corretora/header-top.php
+++ html/wp-content/themes/advanced-corretora/header-top.php
@@ -5,73 +5,86 @@
  */
 ?>
 <header class="header">
-    <div class="container">
-        <div class="content">
-            <div class="logo">
+
+    <div class="content">
+        <div class="logo">
+            <?php
+            if (has_custom_logo()) {
+                echo '<div class="logo-image">';
+                the_custom_logo();
+                echo '</div>';
+            } else {
+                if (is_front_page() && is_home()) : ?>
+                    <h1 class="logo-text">
+                        <a href="<?php echo esc_url(home_url('/')); ?>" rel="home" class="header__logo__link"><?php bloginfo('name'); ?></a>
+                    </h1>
+                <?php else : ?>
+                    <div class="logo-text">
+                        <a href="<?php echo esc_url(home_url('/')); ?>" rel="home" class="header__logo__link"><?php bloginfo('name'); ?></a>
+                    </div>
+                <?php endif; ?>
+            <?php } ?>
+        </div>
+
+        <!-- Botão do menu hambúrguer -->
+        <button class="hamburger-menu" aria-label="Menu" aria-expanded="false">
+            <span class="hamburger-icon">
                 <?php
-                if (has_custom_logo()) {
-                    echo '<div class="logo-image">';
-                    the_custom_logo();
-                    echo '</div>';
-                } else {
-                    if (is_front_page() && is_home()) : ?>
-                        <h1 class="logo-text">
-                            <a href="<?php echo esc_url(home_url('/')); ?>" rel="home" class="header__logo__link"><?php bloginfo('name'); ?></a>
-                        </h1>
-                    <?php else : ?>
-                        <div class="logo-text">
-                            <a href="<?php echo esc_url(home_url('/')); ?>" rel="home" class="header__logo__link"><?php bloginfo('name'); ?></a>
-                        </div>
-                    <?php endif; ?>
-                <?php } ?>
+                echo file_get_contents(get_template_directory() . '/assets/icons/icon_menu_list.svg');
+                ?>
+            </span>
+        </button>
+
+        <div class="menu-container">
+            <span class="close-icon">
+                <?php
+                echo file_get_contents(get_template_directory() . '/assets/icons/icon_close.svg');
+                ?>
+            </span>
+            <div class="menu">
+                <?php
+                if (has_nav_menu('top-menu')) {
+                    wp_nav_menu(
+                        array(
+                            'theme_location' => 'top-menu',
+                            'menu_class' => 'list',
+                            'container' => false,
+                            'fallback_cb' => false,
+                            'items_wrap' => '<ul id="%1$s" class="%2$s">%3$s</ul>',
+                            'link_class' => 'item__link',
+                            'walker' => new Advanced_Corretora_Walker_Nav_Menu(),
+                            'depth' => 3
+                        )
+                    );
+                }
+                ?>
+                <?php
+                $cta_text = carbon_get_theme_option('crb_header_cta_text');
+                $cta_url = carbon_get_theme_option('crb_header_cta_url');
+                $cta_target = carbon_get_theme_option('crb_header_cta_target') ?: '_self';
+                if ($cta_text && $cta_url) {
+                ?>
+                    <div class="cta">
+                        <a href="<?php echo esc_url($cta_url); ?>" class="link" target="<?php echo esc_attr($cta_target); ?>"><?php echo esc_attr($cta_text); ?></a>
+                    </div>
+                <?php
+                }
+                ?>
             </div>
+        </div>
 
-            <!-- Botão do menu hambúrguer -->
-            <button class="hamburger-menu" aria-label="Menu" aria-expanded="false">
-                <span class="hamburger-icon">
-                    <?php
-                    echo file_get_contents(get_template_directory() . '/assets/icons/icon_menu_list.svg');
-                    ?>
-                </span>
-            </button>
-
-            <div class="menu-container">
-                <span class="close-icon">
-                    <?php
-                    echo file_get_contents(get_template_directory() . '/assets/icons/icon_close.svg');
-                    ?>
-                </span>
-                <div class="menu">
-                    <?php
-                    if (has_nav_menu('top-menu')) {
-                        wp_nav_menu(
-                            array(
-                                'theme_location' => 'top-menu',
-                                'menu_class' => 'list',
-                                'container' => false,
-                                'fallback_cb' => false,
-                                'items_wrap' => '<ul id="%1$s" class="%2$s">%3$s</ul>',
-                                'link_class' => 'item__link',
-                                'walker' => new Advanced_Corretora_Walker_Nav_Menu(),
-                                'depth' => 3
-                            )
-                        );
-                    }
-                    ?>
-                    <?php
-                    $cta_text = carbon_get_theme_option('crb_header_cta_text');
-                    $cta_url = carbon_get_theme_option('crb_header_cta_url');
-                    $cta_target = carbon_get_theme_option('crb_header_cta_target') ?: '_self';
-                    if ($cta_text && $cta_url) {
-                    ?>
-                        <div class="cta">
-                            <a href="<?php echo esc_url($cta_url); ?>" class="link" target="<?php echo esc_attr($cta_target); ?>"><?php echo esc_attr($cta_text); ?></a>
-                        </div>
-                    <?php
-                    }
-                    ?>
-                </div>
+        <div class="deskButtonArea">
+            <div class="search">
+                <button class="search-button">
+                    <span class="search-icon">
+                      b
+                    </span>
+                </button>
+            </div>
+            <div class="cta">
+                <a href="<?php echo esc_url($cta_url); ?>" class="link" target="<?php echo esc_attr($cta_target); ?>"><?php echo esc_attr($cta_text); ?></a>
             </div>
         </div>
     </div>
+
 </header>
